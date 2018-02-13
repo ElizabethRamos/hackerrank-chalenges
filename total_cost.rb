8f6438057c8e68685fc87720cfbb63f22baf8dd8
Elizabeth Ramos <elizabethlopesramos@gmail.com>
2018-02-13 09:49:57 +0200

    Total cost meal

--- total_cost.rb
+++ total_cost.rb
@@ -1,5 +1,5 @@
 
-#Challenge:
+#Second Challenge of the Hackerrank 30 Days of code Challenge.
 
 #Given the meal price (base cost of a meal), tip percent (the percentage of the meal price being added as tip),
 #and tax percent (the percentage of the meal price being added as tax) for a meal, find and print the meal's total cost.
